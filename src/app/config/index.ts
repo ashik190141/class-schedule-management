eb4086f7a11eff98fdec8bd5df6cba82639574e0
ashik190141 <ashanmahmudashik@gmail.com>
2025-03-14 15:26:10 +0600

    remove environment

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,7 +4,6 @@ import path from "path";
 dotenv.config({ path: path.join((process.cwd(), ".env")) });
 
 export default {
-  environment: process.env.ENVIRONMENT,
   secret: process.env.SECRET,
   secretExpire: process.env.SECRET_EXPIRE,
 };
